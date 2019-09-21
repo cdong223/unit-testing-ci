c45cb388333abb8eeb0ddae6e693727678ec1b2f
Claire Dong <claire.dong@duke.edu>
2019-09-21 15:04:14 -0400

    Modified is_tachycardic function to check for 'tachycardic' regardless of capitalization and trailing/leading spaces & punctuations

--- tachycardia.py
+++ tachycardia.py
@@ -1,11 +1,18 @@
-def is_tachycardiac(w):
-    print("Entered is_tachycardiac function")
+from string import punctuation
+
+
+def is_tachycardic(w):
+    str = "tachycardic"
+    removable = punctuation + " "
+    w = w.strip(removable) # remove all leading/trailing spaces & punctuation
+    w = w.lower() # make all letters lowercase
+    print(w == str)
     return
 
 
 def user_interface():
-    word = input("Enter a word: ")
-    is_tachycardiac(word)
+    word = input("Enter a word(enter 9 to quit): ")
+    is_tachycardic(word)
     return
 
 if __name__ == "__main__":
